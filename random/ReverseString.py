6852df6bb1ad2203a09efea4a8f8b6f86c67c5af
mostafaradwan1 <mostafaradwan1001@gmail.com>
2020-05-14 17:52:44 +0200

    added reverse 1

--- random/ReverseString.py
+++ random/ReverseString.py
@@ -1,8 +1,19 @@
+# version 1
 def reverse(stri):
     reversed=""
     for i in range(1,len(stri)+1):
         reversed+=stri[-i]
 
     return reversed
+    
+
+# version 2
+
+def reverse1(stri):
+    reversed=''
+    for char in stri:
+        reversed=char +reversed
+    return reversed
+
 
 print(reverse("mostafa"))
